0ac991a1258f82379932a0483c4bce060e414534
Andrew Conti <agc11d@gmail.com>
2013-06-17 12:47:44 -0400

    cleaned table def plus added contacts

--- table_def.py
+++ table_def.py
@@ -12,10 +12,13 @@ Base = declarative_base()
  
 ########################################################################
 class client(Base):
-    """"""
+    """
+    client table
+    """
     __tablename__ = "client"
-    
+    #primary key
     client_id = Column(Integer, Sequence('client_id_seq'), primary_key=True)   
+    
     first_name = Column(String)  
     last_name = Column(String)     
     dob = Column(Date) 
@@ -24,7 +27,7 @@ class client(Base):
 
  
     #----------------------------------------------------------------------
-    def __init__(self, client_id, first_name,  last_name,   dob, phone_number,    account):
+    def __init__(self, client_id, first_name, last_name, dob, phone_number, account):
         """"""
         self.client_id = client_id
         self.first_name = first_name  
@@ -35,13 +38,19 @@ class client(Base):
 
 ########################################################################
 class act_log(Base):
-    """"""
+    """
+    activity log table
+    """
     __tablename__ = "act_log"
-   
+    # handel our ids and foreign relationships
+    #primary key
     transaction_id = Column(Integer, Sequence('client_id_seq'), primary_key=True) 
-    
-    client_id = Column(Integer, ForeignKey("client.client_id"))
+    #forgein key client
+    client_id = Column(Integer, ForeignKey('client.client_id'))
     client = relationship("client", backref=backref("client", order_by=client_id))
+    # forein key contact
+    provider_id = Column(Integer, ForeignKey('contact.provider_id'))
+    contact = relationship("contact", backref=backref("contact", order_by=client_id))
     
     date = Column(Date)    
     descp = Column(Text)   
@@ -52,7 +61,9 @@ class act_log(Base):
  
     #----------------------------------------------------------------------
     def __init__(self, transaction_id, client_id, date, descp, descp_cat, time, unit_price, total):
-        """"""
+        """
+
+        """
         self.transaction_id = transaction_id 
         self.client_id = client_id
         self.date = date    
@@ -61,3 +72,43 @@ class act_log(Base):
         self.time = time   
         self.unit_price = unit_price
         self.total = total
+
+class contact(Base):
+    """
+    activity log table
+    """
+    __tablename__ = 'contact'
+    # handel our ids and foreign relationships
+    #primary key
+    provider_id = Column(Integer, Sequence('provider_id_seq'), primary_key=True) 
+    #forgein key client
+    client_id = Column(Integer, ForeignKey('client.client_id'))
+    client = relationship("client", backref=backref("client", order_by=client_id))
+    # forein key contact
+    # can add transacton id in the future
+    
+    PROVIDER_NAME = Column(Text)    
+    SPECIALITY = Column(Text)   
+    PHONE = Column(Text)   
+    CONTACT_NAME = Column(Text)    
+    ADDRESS = Column(Text) 
+    MISC = Column(Text) 
+    Miles = Column(Float) 
+ 
+    #----------------------------------------------------------------------
+    def __init__(
+                    self, provider_id, client_id, PROVIDER_NAME, SPECIALITY, PHONE, 
+                    CONTACT_NAME, ADDRESS, MISC, Miles):
+        """
+
+        """
+        self.provider_id = provider_id
+        self.client_id = client_id
+        self.PROVIDER_NAME = PROVIDER_NAME 
+        self.SPECIALITY = SPECIALITY
+        self.PHONE = PHONE    
+        self.CONTACT_NAME = CONTACT_NAME   
+        self.ADDRESS = ADDRESS 
+        self.MISC = MISC   
+        self.Miles = Miles
+
